a0f80aebb176ab697eeef1b69fd052deaa4f673b
rpucca@gmail.com <rpucca@gmail.com>
2022-06-26 18:06:15 -0300

    exemplos

--- testeNumber.js
+++ testeNumber.js
@@ -1,3 +1,7 @@
+const teste = "Ricardo";
+console.log(teste + 1);
+console.log(teste * 1); //Nan - Not a number;
+
 const valor = 3000;
 const horas = 176;
 const minhaString = "teste;"
@@ -6,8 +10,6 @@ function ganhoPorHora(salario, horasTrabalhadasNoMes) {
     const salarioHora = (salario / horasTrabalhadasNoMes);
     return salarioHora;
   }   
-
-  lista.sort((a, b) => a - b); // [1, 5, 8, 9, 10, 12, 15]
 
 console.log("Divisão:"+ganhoPorHora(valor,horas));
 console.log("Math.round:"+ Math.round(ganhoPorHora(valor,horas)));
