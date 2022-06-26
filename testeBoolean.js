6b633cd6201a2eda1807bd0ef35781554e853ea2
rpucca@gmail.com <rpucca@gmail.com>
2022-06-26 18:39:00 -0300

    teste de tipos e conversões

--- testeBoolean.js
+++ testeBoolean.js
@@ -6,7 +6,7 @@ console.log(primeiro == segundo);
 console.log(primeiro == terceiro);
 
 /*
-==  haverá uma coerção do valor para que ambos os lados da expressão tenham o mesmo tipo.
-=== não haverá coerção e por isso o código abaixo dará falso:*/
+Dois iguais "=="  haverá uma coerção do valor para que ambos os lados da expressão tenham o mesmo tipo.
+Três iguais "===" não haverá coerção e por isso o código abaixo dará falso:*/
 if(1 ==  '1') console.log('igual'); else console.log('diferente'); //igual
 if(1 === '1') console.log('igual'); else console.log('diferente'); //diferente
