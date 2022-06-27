088f8160d5797f939034d1a0ac1c6acbf23771a8
rpucca@gmail.com <rpucca@gmail.com>
2022-06-27 00:22:04 -0300

    exemplos

--- testeBoolean.js
+++ testeBoolean.js
@@ -10,3 +10,9 @@ Dois iguais "=="  haverá uma coerção do valor para que ambos os lados da expr
 Três iguais "===" não haverá coerção e por isso o código abaixo dará falso:*/
 if(1 ==  '1') console.log('igual'); else console.log('diferente'); //igual
 if(1 === '1') console.log('igual'); else console.log('diferente'); //diferente
+
+//!= e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação, da mesma forma que == e === retornam true ou false.
+if(1 !=  '1') console.log('diferente'); else console.log('igual'); //igual
+if(1 !== '1') console.log('diferente'); else console.log('igual'); //diferente
+
+
